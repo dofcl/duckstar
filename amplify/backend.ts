45f948282fa486f2899dac265c879745d50863ed
Dan O <diydan82@gmail.com>
2025-01-12 20:19:31 +0300

    removed amplify function for webhook put did traditionl lambda to updat ddb direct, will add code to /backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,34 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { updateComputeTaskFunction } from './functions/updateComputeTaskFunction/resource';
-import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
-  storage,
-  updateComputeTaskFunction
+  storage
 });
-
-// Now you can access the Lambda resources
-const functionUrl = backend.updateComputeTaskFunction.resources.lambda.addFunctionUrl({
-  authType: FunctionUrlAuthType.NONE
-});
-
-backend.addOutput({
-  custom: {
-    updateComputeTaskFunction: {
-      functionUrl: functionUrl.url
-    }
-  },
-})
-backend.updateComputeTaskFunction.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    actions: ['dynamodb:UpdateItem','dynamodb:GetItem'],
-    resources: ['*']
-  })
-);
-
-export default backend;
--- amplify/functions/updateComputeTaskFunction/resource.ts
+++
@@ -1,5 +0,0 @@
-import { defineFunction, defineBackend } from '@aws-amplify/backend';
-
-export const updateComputeTaskFunction = defineFunction({
-  entry: './handler.ts',
-})
