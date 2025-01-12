2ad6cd5799d3ea30d92a1ff6deca38ff825fc45b
Dan O <diydan82@gmail.com>
2025-01-12 08:34:48 +0300

    add http route

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,10 +3,33 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { updateComputeTaskFunction } from './functions/updateComputeTaskFunction/resource';
+import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
   updateComputeTaskFunction
 });
+
+// Now you can access the Lambda resources
+const functionUrl = backend.updateComputeTaskFunction.resources.lambda.addFunctionUrl({
+  authType: FunctionUrlAuthType.NONE
+});
+
+backend.addOutput({
+  custom: {
+    updateComputeTaskFunction: {
+      functionUrl: functionUrl.url
+    }
+  },
+})
+backend.updateComputeTaskFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['dynamodb:UpdateItem'],
+    resources: ['*']
+  })
+);
+
+export default backend;
--- amplify/functions/updateComputeTaskFunction/resource.ts
+++ amplify/functions/updateComputeTaskFunction/resource.ts
@@ -1,22 +1,5 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, defineBackend } from '@aws-amplify/backend';
 
-interface EventType {
-  arguments: {
-    taskId: string;
-    status?: string;
-    failed?: boolean;
-    failedReason?: string;
-    finished?: boolean;
-    finishedAt?: string;
-  };
-  data: {
-    ComputeTasks: {
-      update: (input: Record<string, any>) => Promise<any>;
-    };
-  };
-}
-
-// Simplify to just use entry point with the function definition
 export const updateComputeTaskFunction = defineFunction({
-  entry: './handler.ts'
-});
+  entry: './handler.ts',
+})
